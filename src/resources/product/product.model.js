34c40b388714fd3cca9a8e0e2ffd5e51a6d49609
r-aravind <aravind.ar786@gmail.com>
2021-07-10 01:48:51 +0530

    feat: implemented Product Model

---
+++ src/resources/product/product.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    item_type: {
+      type: String,
+      required: true,
+      enum: ["product", "service"],
+      default: "product",
+    },
+    item_name: String,
+    item_code: String,
+    item_description: String,
+    item_images: [String],
+    item_category: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "category",
+    },
+    from_inventory: {
+      type: Boolean,
+      default: false,
+    },
+
+    unit: {
+      type: String,
+      enum: ["feet", "inches", "units", "numbers"],
+    },
+    opening_stock: Number,
+    as_of_date: Date,
+
+    pricing_purchase_price: Number,
+    gst_tax_rate: {
+      type: String,
+      enum: ["exempted", "0", "0.1", "0.25", "3", "5"],
+    },
+    inclusive_of_tax: Boolean,
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
