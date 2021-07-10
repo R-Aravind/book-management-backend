9fba3d7445be03626e6b16cfc1c6c9dc524ef023
r-aravind <aravind.ar786@gmail.com>
2021-07-10 16:07:39 +0530

    feat: added Product controllers

---
+++ src/resources/product/product.controller.js
@@ -0,0 +1,24 @@
+const Product = require("./product.model");
+
+// TODO: return all products
+const getAllProducts = (req, res) => {};
+
+// TODO: return products of a category
+const getByCategory = (req, res) => {};
+
+// TODO: create new product
+const createProduct = (req, res) => {};
+
+// TODO: update product
+const updateProduct = (req, res) => {};
+
+// TODO: delete product
+const deleteProduct = (req, res) => {};
+
+module.exports = {
+  getAllProducts,
+  getByCategory,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
