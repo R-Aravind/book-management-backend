9e9249ac4b861422328069007e83fb32dc782450
r-aravind <aravind.ar786@gmail.com>
2021-07-10 21:09:09 +0530

    feat: added uploadImage method - uploads images to gcp storage

--- src/util/storage.js
+++ src/util/storage.js
@@ -4,13 +4,20 @@ require("dotenv").config();
 
 const initBucket = () => {
   const gc = new Storage({
-    keyFilename: path.join(__dirname, process.env.GCP_KEY),
+    keyFilename: path.join(__dirname, "../../" + process.env.GCP_KEY),
     projectId: process.env.GCP_PROJECT,
   });
 
   return gc.bucket("books-management");
 };
 
+const uploadImage = async (bucket, destination, image, contentType) => {
+  const file = bucket.file(destination);
+  await file.save(image, { contentType: contentType });
+  return file.publicUrl();
+};
+
 module.exports = {
   initBucket,
+  uploadImage,
 };
