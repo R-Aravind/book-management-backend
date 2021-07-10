ff113f3c51baacd0bfe66adbf462f58d0ff7b78f
r-aravind <aravind.ar786@gmail.com>
2021-07-10 20:45:05 +0530

    feat: added multer

--- src/resources/product/product.router.js
+++ src/resources/product/product.router.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const productController = require("./product.controller");
+const Multer = require("multer");
+
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+});
 
 const productRouter = express.Router();
 
 productRouter.get("/", productController.getAllProducts);
-productRouter.post("/", productController.createProduct);
-productRouter.put("/:id", productController.updateProduct);
+productRouter.post(
+  "/",
+  multer.array("images"),
+  productController.createProduct
+);
+productRouter.put(
+  "/:id",
+  multer.array("images"),
+  productController.updateProduct
+);
 productRouter.delete("/:id", productController.deleteProduct);
 productRouter.get("/categories/:id", productController.getByCategory);
 
