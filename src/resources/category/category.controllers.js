4e53da9a59b6f99362675c08718927cdf56abdac
r-aravind <aravind.ar786@gmail.com>
2021-07-10 16:00:10 +0530

    feat: added Category controllers

---
+++ src/resources/category/category.controllers.js
@@ -0,0 +1,12 @@
+const Category = require("./category.model");
+
+// TODO: return all categories
+const getAll = (req, res) => {};
+
+// TODO: create new category
+const create = (req, res) => {};
+
+module.exports = {
+  getAll,
+  create,
+};
